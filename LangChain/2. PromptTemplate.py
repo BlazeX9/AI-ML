f76929322700a444bc35e2983fc4838e875aad05
Abhik <chatterjeeabhik94@gmail.com>
2026-09-07 03:41:55 +0530

    05-09-2026

--- LangChain/2. PromptTemplate.py
+++ LangChain/2. PromptTemplate.py
@@ -3,22 +3,15 @@ import os
 load_dotenv()
 
 from langchain_google_genai import ChatGoogleGenerativeAI
+#from langchain_openai import ChatOpenAI
 llm = ChatGoogleGenerativeAI(api_key=os.getenv("GOOGLE_API_KEY"),model="gemini-3.5-flash-lite")
+#llm = ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY"),model="gpt-4.1-mini")
 
 import streamlit as st
 st.title("Code Generator")
-language_input = st.selectbox(
-    "Select Language",
-    ["Java", "Python"]
-)
-topic_input = st.selectbox(
-    "Select Topic",
-    ["Method Overloading", "Method Overriding", "Inheritance", "Constructor"]
-)
-style_input = st.selectbox(
-    "Select Explanation",
-    ["With Example", "Without Example"]
-)
+language_input = st.selectbox("Language", ["Java", "Python"])
+topic_input = st.selectbox("Topic", ["Method Overloading", "Method Overriding", "Inheritance", "Constructor"])
+style_input = st.selectbox("Explanation", ["With Example", "Without Example"])
 
 from langchain_core.prompts import PromptTemplate
 prompt = PromptTemplate(
