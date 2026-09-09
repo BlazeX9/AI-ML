fa9e58b77b7f5eb87b7d64c1b2afec8e6c071750
Abhik <chatterjeeabhik94@gmail.com>
2026-09-09 19:16:41 +0530

    September 2026

--- LangChain/ChatBot/console.py
+++ LangChain/ChatBot/console.py
@@ -2,18 +2,22 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-from langchain_openai import ChatOpenAI
-#from langchain_google_genai import ChatGoogleGenerativeAI
-llm = ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY"),model="gpt-4.1-mini",temperature=0)
-#llm = ChatGoogleGenerativeAI(api_key=os.getenv("GOOGLE_API_KEY"),model="gemini-3.1-flash-lite",temperature=0)
+#from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+#llm = ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY"),model="gpt-4.1-mini",temperature=0)
+llm = ChatGoogleGenerativeAI(api_key=os.getenv("GOOGLE_API_KEY"),model="gemini-3.5-flash-lite")
 
 from langchain_core.messages import SystemMessage,HumanMessage,AIMessage
 from langchain_core.output_parsers import StrOutputParser
 chain = llm | StrOutputParser()
 
-chat_history = [
-    SystemMessage(content="You are a helpful assistant who replies in simple english and on topic. You are developed in the year 2026")
-]
+prompt = f"""
+You are a helpful assistant who replies in simple english and on topic.
+You are developed in the year 2026.
+You are developed by Abhik Chatterjee.
+"""
+
+chat_history = [SystemMessage(content=prompt)]
 
 while True:
     user_input = input("User: ")
@@ -23,11 +27,11 @@ while True:
         break
     
     if user_input.strip() == "":
-            print("Please ask a question")
-            continue
+        print("Please ask a question")
+        continue
     
     chat_history.append(HumanMessage(content=user_input))
-    
+
     ai_response = chain.invoke(chat_history)
-    chat_history.append(AIMessage(content=ai_response))
     print("Agent:",ai_response)
+    chat_history.append(AIMessage(content=ai_response))
